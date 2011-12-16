88fb6ded60756911886c651740612342c2a1592d
Alexander Balashov <divineforest@gmail.com>
2011-12-16 13:12:15 +0400

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+## Установка
+
+Скопировать [jquery.base64.min, aladdin_etoken.js] к себе на сайт, например, в
+/javascripts/plugins.
+
+Подключить в HTML
+```html
+<script type="text/javascript" src="/javascripts/plugins/jquery.base64.min.js"></script>
+<script type="text/javascript" src="/javascripts/plugins/aladdin_etoken.js"></script>
+```
---
+++ aladdin_etoken.js
@@ -0,0 +1,189 @@
+// require "base64"
+
+function AladdinEtokenModel(usb_id) {
+  this.usb_id = usb_id;
+
+  var info = AladdinEtoken.plugin().getTokenInfo(this.usb_id);
+  this.uid = info[0];
+}
+
+with(AladdinEtokenModel) {
+	prototype.containers = function() {
+    var certs = AladdinEtoken.plugin().getCertificateList(this.usb_id);
+    var container_models = [];
+    $.each(certs, function(i, cert) {
+      container_models.push(new ContainerModel(cert[0], cert[1]));
+    });
+    return container_models;
+	}
+
+  prototype.bind = function(options) {
+    try {
+      AladdinEtoken.plugin().bindToken(this.usb_id, options.pin);
+      options.success();
+    } catch(error) {
+      options.error();
+    }
+  }
+
+  prototype.unbind = function() {
+    AladdinEtoken.plugin().unbindToken();
+  }
+
+  prototype.binded = function() {
+    var state_array = AladdinEtoken.plugin().getLoggedInState();
+    var state_id = state_array[0];
+    var state_connection_id = state_array[1];
+    var state_usb_id = state_array[2];
+
+    return (state_usb_id == this.usb_id) && (state_id > 0);
+  }
+}
+
+function ContainerModel(id, description) {
+	this.id = id;
+	this.description = description;
+}
+
+with(ContainerModel) {
+  prototype.sign = function(content) {
+    var content_encoded = AladdinEtoken.utf8_encode_to_bytes_array(content);
+		var signature_bytes_array = AladdinEtoken.plugin().signData(this.id, content_encoded, false);
+		var signature_base64 = AladdinEtoken._bytes_array_to_base64(signature_bytes_array);
+		return signature_base64;
+	}
+
+	prototype.set_certificate = function(certificate_base64) {
+	  certificate_base64 = AladdinEtoken._clean_base64(certificate_base64);
+		var certificate_string = $.base64.decode(certificate_base64);
+		var certificate_bytes_array = AladdinEtoken.utf8_encode_to_bytes_array(certificate_string);
+		AladdinEtoken.plugin().writeCertificate(this.id, certificate_bytes_array);
+		return true;
+	}
+
+	prototype.certificate = function() {
+		var bytes_array = AladdinEtoken.plugin().readCertificate(this.id);
+		var base64 = AladdinEtoken._bytes_array_to_base64(bytes_array);
+		var base64_parts = base64.match(/.{1,64}/g);
+
+		var standard = "-----BEGIN CERTIFICATE-----\n";
+		$.each(base64_parts, function(index, part) {
+			standard += part+"\n";
+		});
+		standard += "-----END CERTIFICATE-----";
+
+		return standard;
+	}
+}
+
+var AladdinEtoken = {
+  plugin_installed: function() {
+    return (typeof (AladdinEtoken.plugin().valid) != 'undefined' && AladdinEtoken.plugin().valid != null);
+  },
+
+  present: function() {
+    return AladdinEtoken.plugin_installed() ? AladdinEtoken.plugin().getAllTokens().length : false;
+  },
+
+  blank: function() {
+    return !AladdinEtoken.present();
+  },
+
+  onInsert: function(func) {
+    // func();
+  },
+
+  state_change: function(func) {
+    AladdinEtoken.subscribe('loginstatechanged', function (state) {
+      func(state);
+    });
+  },
+
+  inserted: function(func) {
+    AladdinEtoken.subscribe('tokenadded', function (usb_id) {
+      var etoken = new AladdinEtokenModel(usb_id);
+      func(etoken);
+    });
+  },
+
+  removed: function(func) {
+    AladdinEtoken.subscribe('tokenremoved', function (usb_id) {
+      func(usb_id);
+    });
+  },
+
+	all: function() {
+		var token_usb_ids = AladdinEtoken.plugin().getAllTokens();
+		var tokens = [];
+		$.each(token_usb_ids, function(i, token_usb_id) {
+		  tokens.push(new AladdinEtokenModel(token_usb_id));
+		});
+		return tokens;
+	},
+
+  first: function() {
+    var etokens = AladdinEtoken.all();
+    return etokens.length ? etokens[0] : null;
+  },
+
+  find_by_uid: function(uid) {
+    var result;
+    $.each(AladdinEtoken.all(), function(i, etoken) {
+      if (etoken.uid == uid) {
+        result = etoken;
+      }
+    });
+    return result;
+  },
+
+	create_csr: function(dn) {
+    var container_id = AladdinEtoken.plugin().createContainer("A", "I-APEX");
+    var full_dn = ["CN", dn, "C", "RU"];
+    var extensions = ["keyUsage", "digitalSignature"];
+    var csr = AladdinEtoken.plugin().genCSR(container_id, full_dn, extensions);
+    var csr_encoded = AladdinEtoken._bytes_array_to_base64(csr);
+
+    return csr_encoded;
+	},
+
+  // private
+
+	plugin: function() {
+    if (!document.getElementById('etgPlugin')) {
+      $("body").append('<object id="etgPlugin" type="application/x-etokengost" width="0" height="0">');
+    }
+    return document.getElementById('etgPlugin');
+	},
+
+  subscribe: function(name, func) {
+    if (window.addEventListener) {
+      AladdinEtoken.plugin().addEventListener(name, func, false);
+    }
+    else {
+      AladdinEtoken.plugin().attachEvent("on" + name, func);
+    }
+  },
+
+  utf8_encode_to_bytes_array: function(string) {
+    var byte_array = [];
+    for(var i = 0; i < string.length; i++) {
+      byte_array.push(string.charCodeAt(i));
+    }
+
+    return byte_array;
+  },
+
+  _bytes_array_to_base64: function(byte_array) {
+    var plain_string = "";
+    for(var i = 0; i < byte_array.length; i++) {
+      plain_string += String.fromCharCode(byte_array[i]);
+    }
+    var encoded_string = $.base64.encode(plain_string);
+    return encoded_string;
+  },
+
+  _clean_base64: function(base64) {
+    return base64.replace(/[\r\n\s]/g, '');
+  }
+
+};
